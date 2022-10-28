ab9150c3eb1da8771eb2fb9b207084ce6896768b
ArthurHeitmann <art.tec.15@gmail.com>
2022-10-28 07:38:03 +0200

    added BTs for many new file type + wtp helper script

---
+++ tools/miniScripts/wtpDdsExtractor.py
@@ -0,0 +1,29 @@
+import os
+from os import path
+import sys
+
+def extractDds(wtpPath: str):
+	with open(wtpPath, "rb") as f:
+		bytes = f.read()
+
+	start = 0
+	i = 0
+	while True:
+		nextDds = bytes.find(b"DDS ", start + 1)
+		if nextDds == -1:
+			nextDds = len(bytes)
+		outName = path.join(path.dirname(wtpPath), f"{path.basename(wtpPath)}_{i}.dds")
+		with open(outName, "wb") as f:
+			f.write(bytes[start:nextDds])
+		if nextDds == len(bytes):
+			break
+		start = nextDds
+		i += 1
+
+wtpFiles = sys.argv[1:]
+for wtpFile in wtpFiles:
+	try:
+		print("Extracting", path.basename(wtpFile))
+		extractDds(wtpFile)
+	except Exception as e:
+		print(f"Failed to extract {wtpFile}: {e}")
